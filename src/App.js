24c9a6f0670edd552a11ff1ce2598a206bf895c6
Dedar <thededar@gmail.com>
2022-01-22 20:38:51 +0600

    removed unessary import

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
+import React, { useState } from "react";
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
 import format from "date-fns/format";
 import getDay from "date-fns/getDay";
 import parse from "date-fns/parse";
 import startOfWeek from "date-fns/startOfWeek";
-import React, { useState } from "react";
-import { Calendar, dateFnsLocalizer } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
 import "./App.css";
 
 const locales = {
